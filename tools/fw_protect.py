7d21f90d81a50f7f5d975e69b6a20c68f485c906
madHacks10000 <madeline.f.estey@gmail.com>
2020-07-21 19:59:00 +0000

    work on python update tool (Maddie)

--- tools/fw_protect.py
+++ tools/fw_protect.py
@@ -7,22 +7,21 @@ import struct
 
 #open a file with the keys
 def protect_firmware(infile, outfile, version, message):
-    key1 = "0123456789abcdef"
-    
     #read the file with the key-- fix later
-    #with open("secretbuildoutput.txt", 'rb') as k:
-        #key1 = k.read()
+    with open("secretbuildoutput.txt", 'rb') as k:
+        key1 = k.read(16)
+        iv = k.read(16)
     
     #Will encrypt with AES GCM mode
-    cipher_encrypt = AES.new(key1, AES.MODE_GCM)
+    cipher_encrypt = AES.new(key1, AES.MODE_GCM, IV=iv)
     
     # Load firmware binary from infile
     with open(infile, 'rb') as fp:
         firmware = fp.read()
     
-    # Pack version and size into two little-endian shorts
+    #Pack version and size into two little-endian shorts
     #metadata = struct.pack('<HH', version, len(firmware))
-
+    
     #updating with the metadata
     #cipher_encrypt.update(metadata)
     
@@ -30,7 +29,6 @@ def protect_firmware(infile, outfile, version, message):
     firmware_and_message = firmware + message.encode() + b'\00'
 
     #dealing with the the main data
-    nonce = cipher_encrypt.nonce
     ciphertext, tag = cipher_encrypt.encrypt_and_digest(firmware_and_message)
     
     # Write the encrypted message to outfile
@@ -39,7 +37,7 @@ def protect_firmware(infile, outfile, version, message):
     
     #not needed but helpful
     print("Send this info: ")
-    print("Nonce: ".encode("utf-8") + nonce)
+    print("Nonce: ".encode("utf-8") + IV)
     print("Metadata:".encode("utf-8") + meta)
     print("Ciphertext: ".encode("utf-8") + ciphertext)
     print("Tag: ".encode("utf-8") + tag)
