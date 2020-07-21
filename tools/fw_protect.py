898c9be0170fd06df04c9201ee21f5abbe02e77f
madHacks10000 <madeline.f.estey@gmail.com>
2020-07-21 03:50:23 +0000

    design challenge day one work (Maddie)

--- tools/fw_protect.py
+++ tools/fw_protect.py
@@ -7,45 +7,47 @@ import struct
 
 #open a file with the keys
 def protect_firmware(infile, outfile, version, message):
-    #need to somehow pass the key in
-    key1 = b'key'
+    key1 = "0123456789abcdef"
+    
+    #read the file with the key-- fix later
+    #with open("secretbuildoutput.txt", 'rb') as k:
+        #key1 = k.read()
     
     #Will encrypt with AES GCM mode
-    cipher_encrypt = AES.new(key1, AES.MODE_GCM, nonce=nonce)
+    cipher_encrypt = AES.new(key1, AES.MODE_GCM)
     
     # Load firmware binary from infile
     with open(infile, 'rb') as fp:
         firmware = fp.read()
+    
+    # Pack version and size into two little-endian shorts
+    #metadata = struct.pack('<HH', version, len(firmware))
 
+    #updating with the metadata
+    #cipher_encrypt.update(metadata)
+    
     # Append null-terminated message to end of firmware
     firmware_and_message = firmware + message.encode() + b'\00'
 
     #dealing with the the main data
-    ciphertext, tag = cipher_encrypt.encrypt_and_digest(data)
+    nonce = cipher_encrypt.nonce
+    ciphertext, tag = cipher_encrypt.encrypt_and_digest(firmware_and_message)
     
-    # Pack version and size into two little-endian shorts
-    metadata = struct.pack('<HH', version, len(firmware))
-
-    #updating with the metadata
-    cipher_encrypt.update(metadata)
-    
-    # Append firmware and message to metadata
-    firmware_blob = metadata + firmware_and_message
-
-    # Write firmware blob to outfile
+    # Write the encrypted message to outfile
     with open(outfile, 'wb+') as outfile:
-        outfile.write(firmware_blob)
+        outfile.write(ciphertext)
     
-    nonce = cipher_enc.nonce
+    #not needed but helpful
     print("Send this info: ")
     print("Nonce: ".encode("utf-8") + nonce)
     print("Metadata:".encode("utf-8") + meta)
     print("Ciphertext: ".encode("utf-8") + ciphertext)
     print("Tag: ".encode("utf-8") + tag)
     
-    cipher_decrypt = AES.new(key1, AES.MODE_GCM, nonce=nonce)
-    cipher_decrypt.update(meta)
-    plaintext = cipher_decrypt.decrypt_and_verify(ciphertext, tag)
+    #decrypt in a different file
+    #cipher_decrypt = AES.new(key1, AES.MODE_GCM, nonce=nonce)
+    #cipher_decrypt.update(meta)
+    #plaintext = cipher_decrypt.decrypt_and_verify(ciphertext, tag)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Firmware Update Tool')
